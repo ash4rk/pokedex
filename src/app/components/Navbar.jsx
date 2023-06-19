f6775ff0daf9c3a757ca9046252dd4714a8083f4
ash4rk <cool.shark.98@mail.ru>
2023-06-20 02:16:09 +0300

    FIX: Adjust Pokedex text my

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Flex, Box, Button, Text } from '@/app/chakra'
+import { Flex, Box, Button, Text, Center } from '@/app/chakra'
 import { Image } from '@/app/chakra-next-js'
 
 const Navbar = () => {
     return (
         <Flex justifyContent={'space-between'} py={6} alignItems={'center'}>
             <Box position={'relative'} aspectRatio={3 / 2} minHeight={20}>
-                <Text fontWeight={'bold'} fontSize={40} textAlign={'center'} my={4}>
+                <Text fontWeight={'bold'} fontSize={40} textAlign={'center'} my={7}>
                     Pokedex
                 </Text>
             </Box>
